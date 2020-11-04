1b238cd930b1817d7273ef69b839a4d7591e3cc6
Andey Saprykin <asaprykin@griddynamics.com>
2020-11-04 20:36:59 +0400

    Remove: abs path from config

--- flask_app/config/settings.py
+++ flask_app/config/settings.py
@@ -1,4 +1,5 @@
 from configparser import ConfigParser
+import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -6,12 +7,20 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 parser = ConfigParser()
-parser.read("/Users/asaprykin/Documents/projects/flask_capstone/flask_app/config/config.ini")
+parser.read("./config/config.ini")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = parser.get("app_setting", "SQLALCHEMY_DATABASE_URI")
+path_to_data_files = parser.get("app_setting", "SQLALCHEMY_DATABASE_URI")
+if not os.path.exists(path_to_data_files):
+    os.mkdir(path_to_data_files)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.getcwd()}/{path_to_data_files}/database.db"
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = parser.get("app_setting", "SQLALCHEMY_TRACK_MODIFICATIONS") == "True"
 app.config["SECRET_KEY"] = parser.get("app_setting", "SECRET_KEY")
 
-PATH_TO_LOG_FILE = parser.get("app_setting", "PATH_TO_LOG_FILE")
+logger_folder_path = parser.get("app_setting", "PATH_TO_LOG_FILE")
+if not os.path.exists(logger_folder_path):
+    os.mkdir(logger_folder_path)
+
+PATH_TO_LOG_FILE = logger_folder_path + "/log.log"
 
 db = SQLAlchemy(app)
